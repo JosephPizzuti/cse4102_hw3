05b7835454f20b93af40d29db37e2eb205ee8da1
Joseph <jpsocial20@gmail.com>
2026-03-24 22:50:02 -0400

    Added solutions for Task 6 and Task 7

--- lambda.ml
+++ lambda.ml
@@ -17,12 +17,30 @@ let rec pretty_print (e: expr) : string =
         | _ -> "(" ^ (pretty_print e2) ^ ")")
 
 let rec subst (e_to_sub : expr) (x : string) (e_in : expr) : expr =
-  raise Util.Unimplemented
+  match e_in with
+  | Var y             -> if x = y then e_to_sub else Var y
+  | App (e1, e2)      -> App (subst e_to_sub x e1, subst e_to_sub x e2)
+  | Lambda (y, body)  ->
+      if x = y 
+      then Lambda (y, body)
+      else Lambda (y, subst e_to_sub x body)
 
 exception UnboundVariable
 
 let rec step (e: expr) : expr option =
-  raise Util.Unimplemented
+  match e with
+  | Var _         -> raise UnboundVariable
+  | Lambda _      -> None
+  | App (e1, e2)  ->
+      match step e1 with
+      | Some e1' -> Some (App (e1', e2))
+      | None ->
+          match step e2 with
+          | Some e2' -> Some (App (e1, e2'))
+          | None ->
+              match e1 with
+              | Lambda (x, body) -> Some (subst e2 x body)
+              | _ -> None
 
 let rec interp (e : expr) : expr =
   Printf.printf "%s\n%!" (pretty_print e);
